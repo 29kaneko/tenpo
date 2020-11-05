d6eebb1fe2ed740c522e440e48666984f5dcd99e
kaneko <kaneko.yosuke0222@icloud.com>
2020-11-05 16:42:14 +0000

    11.5

--- resources/views/admin/tenpo/create.blade.php
+++ resources/views/admin/tenpo/create.blade.php
@@ -3,8 +3,15 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>ログイン画面</h2>
+                <div class="col align-self-start">
+                <h2>悠然かしや</h2>
+                    <a>{{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary btn-sm" value="新規登録">
+                    </a>
+                </div>
+                <div class="col align-self-center">
+                <h3>ログイン画面</h3>
+                
                 <form action="{{action('Admin\TenpoController@create') }}" method="post" enctype="multipart/form-data">
                 
                     @if (count($errors) > 0)
@@ -14,43 +21,33 @@
                             @endforeach
                         </ul>
                     @endif
-                    <div class="form-group row">
-                        <label class="col-md-2">メールアドレス</label>
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" name="email-address" value="{{ old('email-address') }}">
+
+                    <div class="row">
+                        <label class="col-md-8">メールアドレス、電話番号</label>
+                        <div class="col-8">
+                            <input type="text" class="form-control" name="email-address,telephone-number" value="{{ old('email-address,telephone-number') }}">
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">パスワード</label>
-                        <div class="col-md-6">
+                    <div class="row">
+                        <label class="col-md-8">パスワード</label>
+                        <div class="col-8">
                             <input type="text" class="form-control" name="password" value="{{ old('password') }}">
                         </div>
                     </div>
                     
-                    <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワード（確認のため再度入力してください）</label>
- 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                    <div class="row">
+                            <div class="col-md-8">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
- 
                                     <label class="form-check-label" for="remember">ログイン情報を保存する</label>
                                 </div>
                             </div>
                     </div>
-                    
+        <div class="row">
+                <div class="col align-self-center">    
                     {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="新規登録">
-                    
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="ログイン">
-                    
+                    <input type="submit" class="btn btn-primary btn-lg" value="ログイン">
+                </div>    
                     
                 </form>
             </div>
